18cae5b2c9005f5eafbf0c72915c1f44f8b664c0
Ronald <ronald.syarif@enterprisys.co.id>
2021-05-31 13:30:06 +0700

    double db

---
+++ database/migrations/2021_05_31_061705_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('mysql2')->create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('qty')->nullable();
+            $table->string('harga')->nullable();;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
