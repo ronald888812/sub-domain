b661a99b13bf0cce7d8d29266b81b977d240b5b2
Ronald <ronald.syarif@enterprisys.co.id>
2021-06-02 11:21:33 +0700

    erase

--- app/Http/Middleware/LocalizationMiddleware.php
+++ app/Http/Middleware/LocalizationMiddleware.php
@@ -10,13 +10,10 @@ class LocalizationMiddleware
 
     public function handle(Request $request, Closure $next)
     {
-         // Cek apakah session 'locale' ada?
          if ($request->session()->has('locale')) {
-            // Jika ada, maka set App Locale sesuai nilai yang ada di session 'locale'.
+       
             \App::setLocale($request->session()->get('locale'));
         }
-        
-        // Lanjutkan request.
         return $next($request);
     }
 }
