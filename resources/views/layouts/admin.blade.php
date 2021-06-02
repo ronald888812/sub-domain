28024b70976f5ab505c9668cb5783006a414efaa
Ronald <ronald.syarif@enterprisys.co.id>
2021-06-02 16:16:58 +0700

    change default 8000

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -64,10 +64,7 @@
             </nav>
             <aside class="main-sidebar sidebar-dark-primary elevation-4">
                 <a href="{{ route('request.index') }}" class="brand-link ">
-                    <img src="{{asset('vendor/adminlte/dist/img/AdminLTELogo.png"
-                        alt="AdminLTE"
-                        class="brand-image img-circle elevation-3"
-                        style="opacity:.8')}}">
+                      <img src="{{ asset('dist/img/user4-128x128.jpg') }}" class="img-circle" alt="User Image" width="50" height="50">
                         <span class="brand-text font-weight-light "><b>Admin</b>LTE</span>
                 </a>
                 <div class="sidebar">
