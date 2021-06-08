29ec6df8f52699ddffe7e2754b81c9a065b348d1
Ronald <ronald.syarif@enterprisys.co.id>
2021-06-08 09:27:42 +0700

    seeder

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -22,14 +22,14 @@ class ProductsSeeder extends Seeder
                 'id'  => 1 ,
                 'title_en'=> 'title en 1',
                 'title_id'=> 'title id 1',
-                'desc_id'=> 'descripton id 1' ,
+                'desc_id'=> 'description id 1' ,
                 'desc_en'=> 'description en 1',
             ],
             [
                 'id'  => 2,
                 'title_en'=> 'title en 2',
                 'title_id'=> 'title id 2',
-                'desc_id'=> 'descripton id 2' ,
+                'desc_id'=> 'description id 2' ,
                 'desc_en'=> 'description en 2' 
             ],
         ]);
