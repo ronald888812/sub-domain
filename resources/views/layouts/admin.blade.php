27b73bf033776006b82bed78a0a84d89365a567a
Ronald <ronald.syarif@enterprisys.co.id>
2021-06-11 09:44:36 +0700

    git push

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{str_replace('_', '-', app()->getLocale())}}">
 <head>    
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="yip6ksyOV86QCfYblfVsovsjRy1I32Yo2XDmYHpw">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="http://127.0.0.1:8000/vendor/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="http://127.0.0.1:8000/vendor/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <link rel="stylesheet" href="http://127.0.0.1:8000/vendor/adminlte/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{asset('vendor/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/adminlte/dist/css/adminlte.min.css')}}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-    <link rel="stylesheet" href="/css/admin_custom.css">
+    <link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
+ 
+    <script type="text/javascript" src="DataTables/datatables.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.25/datatables.min.css"/>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.25/datatables.min.js"></script>
+    @yield('additional-css')
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('bower_components/font-awesome/css/font-awesome.min.css') }}">
+
 </head>
 <body class="sidebar-mini" >
         <div class="wrapper">    
@@ -26,6 +33,49 @@
                         </a>
                     </li>
           
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    {{-- <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ __('home.switch_language') }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item {{ app()->getLocale() == 'en' ? 'active' : '' }}" href="{{ route('localization.switch', 'en') }}">English</a>
+                            <a class="dropdown-item {{ app()->getLocale() == 'id' ? 'active' : '' }}" href="{{ route('localization.switch', 'id') }}">Bahasa Indonesia</a>
+                        </div>
+                    </li> --}}
+
+
+                    <li class = "nav-item dropdown" id = "nav-lang">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                            {{Config::get('languages') [App::getLocale()]}}
+                            <span class = "caret"></span>
+                        </a>
+                        <ul class = "dropdown-menu">
+                            @foreach ((array) Config::get ('languages') as $lang => $language)
+                            @if ($lang != App::getLocale())
+                                <li>
+                                    <a href="{{route('lang.switch', $lang)}} "> {{$language}} </a>
+                                </li>
+                            @endif
+                            @endforeach
+                        </ul>
+                    </li>
+                    {{-- <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            {{ Config::get('languages')[App::getLocale()] }}
+                        </a>
+                        <ul class="dropdown-menu">
+                            @foreach (Config::get('languages') as $lang => $language)
+                                @if ($lang != App::getLocale())
+                                    <li>
+                                        <a href="{{ route('lang.switch', $lang) }}">{{$language}}</a>
+                                    </li>
+                                @endif
+                            @endforeach
+                        </ul>
+                    </li> --}}
                 </ul>
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item dropdown user-menu">
@@ -51,41 +101,37 @@
                 </ul>
             </nav>
             <aside class="main-sidebar sidebar-dark-primary elevation-4">
-                <a href="http://127.0.0.1:8000/home" class="brand-link ">
-                    <img src="http://127.0.0.1:8000/vendor/adminlte/dist/img/AdminLTELogo.png"
-                        alt="AdminLTE"
-                        class="brand-image img-circle elevation-3"
-                        style="opacity:.8">
+                <a href="{{ route('request.index') }}" class="brand-link ">
+                      <img src="{{ asset('dist/img/user4-128x128.jpg') }}" class="img-circle" alt="User Image" width="50" height="50">
                         <span class="brand-text font-weight-light "><b>Admin</b>LTE</span>
                 </a>
                 <div class="sidebar">
                     <nav class="mt-2">
                         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
-                            <li  class="nav-item">
+                            {{-- <li  class="nav-item">
                                 <a class="nav-link" href="{{ route('request.index') }}">
                                     <i class="far fa-fw fa-file "></i>
-                                    <p>Request</p>
+                                    <p>{{ __('home.request') }}</p>
                                 </a>
-                            </li>
+                            </li> --}}
                             <li  class="nav-item">
-                                <a class="nav-link" href="{{ route('user.index') }}"
+                                <a class="nav-link" href="{{ route('user.index') }}">
                                     <i class="fas fa-fw fa-user "></i>
-                                    <p>Profile</p>
+                                    <p>{{ __('home.profile') }}</p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('products.index', ['subdomain' => Auth::user()->role]) }}"
+                                <a class="nav-link" href="{{ route('products.index', ['subdomain' => Auth::user()->role]) }}">
                                     <i class="fas fa-fw fa-user"></i>
-                                    <p>Domain Product</p>
+                                    <p>{{ __('home.domain_product') }} </p>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('tipes.index', ['subdomain' => Auth::user()->role]) }}"
+                                <a class="nav-link" href="{{ route('tipes.index', ['subdomain' => Auth::user()->role]) }}">
                                     <i class="fas fa-fw fa-user"></i>
                                     <p>Domain Tipe </p>
                                 </a>
                             </li>
-                             <li class="nav-header ">ACCOUNT SETTINGS</li>
                         </ul>
                     </nav>
                 </div>
@@ -94,10 +140,11 @@
                 @yield('content')
             </div>
         </div>
-        <script src="http://127.0.0.1:8000/vendor/jquery/jquery.min.js"></script>
-        <script src="http://127.0.0.1:8000/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="http://127.0.0.1:8000/vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-        <script src="http://127.0.0.1:8000/vendor/adminlte/dist/js/adminlte.min.js"></script>
+        <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+        <script src="{{asset('vendor/adminlte/dist/js/adminlte.min.js')}}"></script>
         <script> console.log('Hi!'); </script>
+        @yield('additional-js')
 </body>
 </html>
