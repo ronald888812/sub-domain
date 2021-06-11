bcc7691e553c6caf101a60534e37442ef6999b0d
Ronald <ronald.syarif@enterprisys.co.id>
2021-06-11 09:54:29 +0700

    push

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -47,21 +47,7 @@
                     </li> --}}
 
 
-                    <li class = "nav-item dropdown" id = "nav-lang">
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                            {{Config::get('languages') [App::getLocale()]}}
-                            <span class = "caret"></span>
-                        </a>
-                        <ul class = "dropdown-menu">
-                            @foreach ((array) Config::get ('languages') as $lang => $language)
-                            @if ($lang != App::getLocale())
-                                <li>
-                                    <a href="{{route('lang.switch', $lang)}} "> {{$language}} </a>
-                                </li>
-                            @endif
-                            @endforeach
-                        </ul>
-                    </li>
+                    
                     {{-- <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             {{ Config::get('languages')[App::getLocale()] }}
